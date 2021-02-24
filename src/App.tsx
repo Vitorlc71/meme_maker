a210a8046b97c3860b4fa4e553bb5e0e174c0bc1
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-02-24 17:07:27 -0300

    exclude route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 import Home from './screens/Home'
 
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
-    </Router>
+    <>
+      <Home />
+    </>
   );
 }
 
